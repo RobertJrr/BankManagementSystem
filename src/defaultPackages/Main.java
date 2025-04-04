f8be456847d12b93a94012832e2dac7f976c5b88
Robert M <99871728+Robert010600@users.noreply.github.com>
2025-04-03 18:48:15 -0700

    Testing the waters

---
+++ src/defaultPackages/Main.java
@@ -0,0 +1,9 @@
+package defaultPackages;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Sup brah");
+        System.out.println("Just testing the waters");
+    }
+}
