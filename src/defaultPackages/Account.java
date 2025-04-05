5c02d78d3cd05789337557e6ac24a91574a2e959
Robert M <99871728+Robert010600@users.noreply.github.com>
2025-04-05 01:21:10 -0700

    Working on account class

--- src/defaultPackages/Account.java
+++ src/defaultPackages/Account.java
@@ -1,6 +1,50 @@
 package defaultPackages;
 
 
-//Account class, will store elements
+//Account class
+/*Consists of attributes such as:
+    name,
+    accountNumber,
+    balance
+
+ Will contain setter and getter methods and other helpful methods
+*/
+
 public class Account {
+
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    public Account(String name, String accountNumber,double balance){
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void 
 }
--- src/defaultPackages/Main.java
+++ src/defaultPackages/Main.java
@@ -3,6 +3,6 @@ package defaultPackages;
 public class Main {
 
     public static void main(String[] args) {
-        
+
     }
 }
